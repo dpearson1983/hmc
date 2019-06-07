3d5fa36a6344d9a6b0adff8d23811cdafec3eea5
David Pearson <dpearson@localhost.localdomain>
2019-06-07 14:47:38 -0500

    Start of class framework

---
+++ include/hmc.hpp
@@ -0,0 +1,21 @@
+#ifndef _HMC_HPP_
+#define _HMC_HPP_
+
+#include <likelihood.hpp>
+
+template <typename T> class hmc{
+    std::vector<double> theta_0, theta_i, vel;
+    double chisq_0, chisq_i;
+    double dt;
+    int steps;
+    
+    void integrate();
+    
+    public:
+        hmc(std::vector<double> theta_0, double dt, int steps, double kick);
+        
+        void run(int numReals);
+};
+
+#endif
+        
---
+++ include/likelihood.hpp
@@ -0,0 +1,30 @@
+#ifndef _HMC_HPP_
+#define _HMC_HPP_
+
+template <typename T> class likelihood{
+    std::vector<T> x;
+    std::vector<double> data, model;
+    
+    void calculateModel(std::vector<double> &theta);
+    
+    double getChisq();
+    
+    public:
+        likelihood(std::vector<T> &x, std::vector<double> &data, std::vector<double> &model);
+        
+        double getLikelihood(std::vector<double> &theta);
+        
+        double getNegLogLike(std::vector<double> &theta);
+        
+};
+
+/* This function is to be defined by the user. Here you're model should be able to be calculated soley
+ * on based on the supplied parameter vector. If you need to, you should add more data members and functions
+ * in order to accomplish this calculation. For example, if you need to create a cubic spline you can add
+ * new data members to store that information, and modify the constructor to initialize that object.
+ */
+void likelihood::calculateModel(std::vector<double> &theta) {
+    
+}
+
+#endif
