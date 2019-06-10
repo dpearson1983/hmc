549d8d47d95860d9c6c4ab5dcd19e9a231b8ae3f
David Pearson <dpearson@localhost.localdomain>
2019-06-10 14:30:08 -0500

    Update repo with progress

--- include/hmc.hpp
+++ include/hmc.hpp
@@ -4,19 +4,94 @@
 #include <likelihood.hpp>
 
 template <typename T> class hmc{
-    std::vector<double> theta_0, theta_i, vel;
-    double chisq_0, chisq_i;
+    std::vector<std::vector<double>> theta_0, theta_i, vel, acc, kick;
+    std::vector<double> chisq_0, chisq_i;
+    std::vector<int> trials;
     double dt;
-    int steps;
+    int steps, walkers;
+    std::mt19937_64 gen;
+    std::normal_distribution<double> velDist;
+    std::uniform_real_distribution<double> trialDist;
     likelihood<T> L;
     
-    void integrate();
+    void kicker(int tid);
+    
+    void updateVel(double t, int tid);
+    
+    void updatePos(double t, int tid);
+    
+    void updateACC(int tid);
+    
+    void integrate(int tid);
+    
+    bool trial();
     
     public:
-        hmc(std::vector<double> theta_0, double dt, int steps, double kick);
+        hmc();
+        
+        hmc(std::vector<double> theta_0, double dt, int steps, std::vector<double> &kick, likelihood<T> &L);
+        
+        void init(std::vector<double> theta_0, double dt, int steps, std::vector<double> & kick, 
+                  likelihood<T> &L);
         
         void run(int numReals);
 };
+
+void hmc<T>::kicker(int tid) {
+    for (size_t i = 0; i < this->theta_0[tid].size(); ++i) {
+        this->vel[tid][i] = this->kick[i]*this->velDist(this->gen);
+    }
+}
+
+void hmc<T>::updateVel(double t, int tid) {
+    for (size_t i = 0; i < this->theta_i[tid].size(); ++i) {
+        this->vel[tid][i] += 0.5*this->acc[tid][i]*t;
+    }
+}
+
+void hmc<T>::updatePos(double t, int tid) {
+    for (size_t i = 0; i < this->theta_i[tid].size(); ++i) {
+        this->theta_i[tid][i] += this->vel[tid][i]*t + 0.5*this->acc[tid][i]*t;
+    }
+}
+
+void hmc<T>::updateAcc(int tid) {
+    double h = 1E-6;
+    for (size_t i = 0; i < this->theta_i[tid].size(); ++i) {
+        this->theta_i[tid][i] += 0.5*h;
+        double f_p = this->L.getNegLogLike(this->theta_i[tid], tid);
+        this->theta_i[tid][i] -= h;
+        double f_m = this->L.getNegLogLike(this->theta_i[tid], tid);
+        this->theta_i[tid][i] += 0.5*h;
+        this->acc[tid][i] = (f_p - f_m)/h;
+    }
+}
+
+void hmc<T>::integrate(int tid) {
+    this->theta_i = this->theta_0;
+    hmc<T>::kicker(tid);
+    
+    for (int i = 0; i < steps; ++i) {
+        hmc<T>::updateVel(0.5*this->dt, tid);
+        hmc<T>::updatePos(this->dt, tid);
+        hmc<T>::updateAcc(tid);
+        hmc<T>::updateVel(0.5*this->dt, tid);
+    }
+}
+
+bool hmc<T>::trial(int tid) {
+    this->trials[tid]++;
+    bool moved = false;
+    hmc<T>::integrate(tid);
+    double L = exp(0.5*(this->chisq_0[tid] - this->chisq_i[tid]);
+    double R = this->trial_dist(this->gen);
+    
+    if (L > R) {
+        this->theta_0[tid] = this->theta_i[tid];
+        this->chisq_0[tid] = this->chisq_i[tid];
+    }
+    return moved;
+}
 
 #endif
         
--- include/likelihood.hpp
+++ include/likelihood.hpp
@@ -1,22 +1,41 @@
 #ifndef _HMC_HPP_
 #define _HMC_HPP_
 
+#include <vector>
+#include <string>
+#include <cmath>
+
 template <typename T> class likelihood{
+    // Default data members
     std::vector<T> x;
-    std::vector<double> data, model;
+    std::vector<double> data;
+    std::vector<std::vector<double>> model;
+    std::vector<std::vector<double>> Psi;
+    int walkers;
     
-    void calculateModel(std::vector<double> &theta);
+    // User defined data members
     
-    double getChisq();
+    
+    // Private member functions, three need to be dilled in by the user. If additional functions are needed
+    // to facilitate the calculation of the model, those should be defined here, and implemented below.
+    void calculateModel(std::vector<double> &theta, int tid = 0); // User defined
+    
+    void readDataFile(std::string file); // User defined
+    
+    void readCovarFile(std::string file); // User defined
+    
+    double getChisq(int tid = 0);
     
     public:
         likelihood();
         
-        void init(
+        likelihood(std::string dataFile, std::string covarFile, int walkers);
         
-        double getLikelihood(std::vector<double> &theta);
+        void init(std::string dataFile, std::string covarFile, int walkers);
         
-        double getNegLogLike(std::vector<double> &theta);
+        double getLikelihood(std::vector<double> &theta, int tid = 0);
+        
+        double getNegLogLike(std::vector<double> &theta, int tid = 0);
         
 };
 
@@ -25,8 +44,60 @@ template <typename T> class likelihood{
  * in order to accomplish this calculation. For example, if you need to create a cubic spline you can add
  * new data members to store that information, and modify the constructor to initialize that object.
  */
-void likelihood::calculateModel(std::vector<double> &theta) {
+void likelihood<T>::calculateModel(std::vector<double> &theta, int tid) {
     
+}
+
+/* This function should read in the data to fit the model too. It should approiately store the x values
+ * and the data in the built in data members. The templated type allows you to use custom structs for
+ * the x values in case you have more than one independent variable.
+ */
+void likelihood<T>::readDataFile(std::string file) {
+    
+}
+
+/* This function should read in the covariance matrix for your data, invert it and store it into the Psi data
+ * member.
+ */
+void likelihood<T>::readCovarFile(std::string file) {
+    
+}
+
+double likelihood<T>::getChisq() {
+    double chisq = 0.0;
+    for (size_t i = 0; i < this->data.size(); ++i) {
+        for (size_t j = i; i < this->data.size(); ++j) {
+            chisqs += (this->data[i] - this->model[tid][i])*this->Psi[i][j]*(this->data[j] - this->model[tid][j]);
+        }
+    }
+    return chisq;
+}
+
+likelihood<T>::likelihood() {
+    this->walkers = 0;
+}
+
+likelihood<T>::likelihood(std::string dataFile, std::string covarFile, int walkers) {
+    likelihood<T>::init(dataFile, covarFile, int walkers);
+}
+
+void likelihood<T>::init(std::string dataFile, std::string covarFile, int walkers) {
+    likelihood<T>::readDataFile(dataFile);
+    likelihood<T>::readCovarFile(covarFile);
+    
+    this->walkers = walkers;
+    this->model = std::vector<std::vector<double>>(walkers, std::vector<double>(this->data.size()));
+}
+
+double likelihood<T>::getLikelihood(std::vector<double> &theta, int tid) {
+    likelihood<T>::calculateModel(theta, tid);
+    double chisq = likelihood<T>::getChisq(tid);
+    return std::exp(-0.5*chisq);
+}
+
+double likelihood<T>::getNegLogLike(std::vector<double> &theta, int tid) {
+    likelihood<T>::calculateModel(theta, tid);
+    return likelihood<T>::getChisq(tid);
 }
 
 #endif
